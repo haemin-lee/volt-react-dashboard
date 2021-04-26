db99679f7e4c2762061dfb4a9dd29e9c78425e50
Jenna Lau <jennanla@usc.edu>
2021-04-25 22:19:31 -1000

    added warnings for all violations

--- src/pages/dashboard/ModalFoodItem.js
+++ src/pages/dashboard/ModalFoodItem.js
@@ -1,4 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
+import { 
+  VIOLATION_PRONUNCIATION,
+  VIOLATION_NO_ORDINARY_INGREDIENTS,
+  VIOLATION_HEALTH_CLAIM_WORDS,
+  VIOLATION_UNIVERSAL_NAME,
+  VIOLATION_AVOID_HFCS,
+  VIOLATION_ADDED_SUGAR
+} from './constants'
 
 const ModalFoodItem = (props) => {
 
@@ -6,14 +14,54 @@ const ModalFoodItem = (props) => {
 
     // display warning on modal popup
     function findWarning() {
-        // food rule violation: recalled food item
-        if (currItem[0].recall) {
-          return <div>Recall Found</div>;
+
+        try{
+          let warnings = []
+          if (currItem[0].ingredientNumber > 5) {
+            warnings.push(<span>Food Rule Violation: More than 5 ingredients found</span>);
+          }
+  
+          let violations = currItem[0].violationsList;
+          if( violations == null || violations === undefined || typeof violations === "undefined"){
+            return;
+          }
+
+          for(let i = 0; i < violations.length; i++){
+            
+            let violationType = violations[i]
+            if(violationType === null){
+                break;
+            }
+
+            if(violation.name === "VIOLATION_PRONUNCIATION"){
+              warnings.push( <span>WARNING: {VIOLATION_PRONUNCIATION}</span>)
+            }
+            if(violation.name === "VIOLATION_NO_ORDINARY_INGREDIENTS"){
+                warnings.push( <span>WARNING: {VIOLATION_NO_ORDINARY_INGREDIENTS}</span>)
+            }
+            if(violation.name === "VIOLATION_HEALTH_CLAIM_WORDS"){
+                warnings.push( <span>WARNING: {VIOLATION_HEALTH_CLAIM_WORDS}</span>)
+            }
+            if(violation.name === "VIOLATION_UNIVERSAL_NAME"){
+                warnings.push( <span>WARNING: {VIOLATION_UNIVERSAL_NAME}</span> )
+            }
+            if(violation.name === "VIOLATION_AVOID_HFCS"){
+                warnings.push( <span>WARNING: {VIOLATION_AVOID_HFCS}</span>)
+            }
+            if(violation.name === "VIOLATION_ADDED_SUGAR"){
+              warnings.push( <span>WARNING: {VIOLATION_ADDED_SUGAR}</span> )
+            }
+
+            return warnings;
+
+
+          }
+
+        } catch(err) {
+          console.log(err);
+          return;
         }
-        // food rule violation: more than 5 ingredients found
-        if (currItem[0].ingredientNumber > 5) {
-          return <span>Food Rule Violation: More than 5 ingredients found</span>;
-        }
+        
 
         // TODO: filter through currItem[0].violationsList
 
